a9b6b347c405bc520621315c5b283e40b04f8714
miguelcaravantes <miguel.caravantes0@gmail.com>
2026-05-25 20:01:06 -0600

    Revert "feat: implement automatic pruning of obsolete entities in __init__.py"
    This reverts commit 0d1eaff9e0578af26d46ea9f2c12972172580eed.

--- custom_components/staggassistant/__init__.py
+++ custom_components/staggassistant/__init__.py
@@ -1,33 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
 from .const import DOMAIN, PLATFORMS, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
 from .coordinator import StaggLinkCoordinator
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Setup StaggAssistant über die UI."""
     
-    # Prune obsolete entities from the registry
-    ent_reg = er.async_get(hass)
-    obsolete_entities = [
-        "switch.stagg_kettle_ekg_pro_hold_mode",
-        "switch.stagg_kettle_ekg_pro_chime",
-        "switch.stagg_kettle_ekg_pro_schedule",
-        "select.stagg_kettle_ekg_pro_volume_level",
-        "sensor.stagg_kettle_ekg_pro_hold_time",
-        "sensor.stagg_kettle_ekg_pro_altitude",
-        "sensor.stagg_kettle_ekg_pro_language",
-        "sensor.stagg_kettle_ekg_pro_chime_volume",
-        "sensor.stagg_kettle_ekg_pro_clock_mode",
-        "sensor.stagg_kettle_ekg_pro_temperature_units",
-        "sensor.stagg_kettle_ekg_pro_schedule_mode",
-    ]
-    for entity_id in obsolete_entities:
-        # The entity_id might have variations based on the name, so we check the unique_id if possible,
-        # but since we want to prune the specific unavailable ones:
-        if ent_reg.async_get(entity_id):
-            ent_reg.async_remove(entity_id)
-
     # Intervall aus der Config lesen, oder Default nutzen falls fehlt
     interval = entry.data.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
     
@@ -38,7 +16,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Daten in HA speichern
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     
-    # Plattformen laden
+    # Plattformen (Climate) laden
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
